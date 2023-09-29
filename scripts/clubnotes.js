d2acd37f2fe3247e840e8ebc63ebf6b5c03e97fc
DaltonJabberwo <71056017+DaltonJabberwo@users.noreply.github.com>
2023-09-29 16:38:26 -0600

    Began format for notes pages. Still need to split semesters and fix screen sizing issues. Also formatting to be done, but not very helpful until we have a general theme picked.

--- scripts/clubnotes.js
+++ scripts/clubnotes.js
@@ -4,7 +4,6 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
-
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
